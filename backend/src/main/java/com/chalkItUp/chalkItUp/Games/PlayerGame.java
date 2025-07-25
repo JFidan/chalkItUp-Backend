001e46107d6b8d8c232cda998050696cb0e422cd
jonathan.fidan@adesso.de <jonathan.fidan@adesso.de>
2025-07-25 12:15:14 +0200

    updated Game

--- backend/src/main/java/com/chalkItUp/chalkItUp/Games/PlayerGame.java
+++ backend/src/main/java/com/chalkItUp/chalkItUp/Games/PlayerGame.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class PlayerGame {
-    public String userID;
+    public String userId;
     public int team;
     public boolean isWinner;
 }
